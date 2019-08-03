146ad2ccc9a686cbdbda4cbcacead0ca13a1166c
Marcelo Abrão da Silva <masilvasql@gmail.com>
2019-08-03 12:21:37 -0300

    respondendo com axios

---
+++ EXERCICIOS/devolverArquivos.js
@@ -0,0 +1,20 @@
+const env = require('../.env')
+const Telegraf = require('telegraf')
+const axios = require('axios')
+const bot = new Telegraf(env.token)
+
+/* Respondendo com dados da api do próprio telegram com axios */
+
+bot.on('voice', async ctx =>{
+    const id = ctx.update.message.voice.file_id
+    const res = await axios.get(`${env.apiUrl}/getFile?file_id=${id}`)
+    ctx.replyWithVoice({url: `${env.apiFileUrl}/${res.data.result.file_path}`})
+})
+
+bot.on('photo', async ctx=>{
+    const id = ctx.update.message.photo[0].file_id
+    const res = await axios.get(`${env.apiUrl}/getFile?file_id=${id}`)
+    ctx.replyWithPhoto({url: `${env.apiFileUrl}/${res.data.result.file_path}`})
+})
+
+bot.startPolling()
